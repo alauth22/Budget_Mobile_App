0f0070b32e0c4e89907f41ff282c2bb4711c7d2e
alauth22 <amelialauth@gmail.com>
2024-12-07 13:54:52 -0600

    Corrected the update method

--- app/src/main/java/com/example/finalproject/Activity/BudgetItems.java
+++ app/src/main/java/com/example/finalproject/Activity/BudgetItems.java
@@ -2,6 +2,7 @@ package com.example.finalproject.Activity;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
@@ -49,6 +50,8 @@ public class BudgetItems extends AppCompatActivity {
             finish();
         });
 
+        //because no buttons have been clicked on, we are going to automatically set the MoreIncome to disabled.
+
 
         //get my edit text fields
         Income = findViewById(R.id.Income);
@@ -68,7 +71,6 @@ public class BudgetItems extends AppCompatActivity {
         MoreIncome = findViewById(R.id.MoreIncome);
 
 
-
         //my buttons
         updateButton = findViewById(R.id.Update);
         refreshButton = findViewById(R.id.Refresh);
@@ -77,27 +79,21 @@ public class BudgetItems extends AppCompatActivity {
 
 
         //these are my checks to ensure that the user cannot just click on buttons here and there.
-        String incomePresent = dbAssist.getStartingIncome(userID);
+        String incomePresent = dbAssist.getIncome(userID);
         if(incomePresent.isEmpty()){
             addButton.setEnabled(true);
-            Income.setFocusable(true);
-            MoreIncome.setFocusable(false);
+            updateButton.setEnabled(false);
+            Income.setEnabled(true);
+            MoreIncome.setEnabled(false);
         }
         else
         {
             addButton.setEnabled(false);
-            Income.setFocusable(false);
-            MoreIncome.setFocusable(true);
+            updateButton.setEnabled(true);
+            Income.setEnabled(false);
+            MoreIncome.setEnabled(true);
         };
 
-        //user cannot update their budget unless they have added one before.
-        if(incomePresent.isEmpty()){
-            updateButton.setEnabled(false);
-        }
-        else
-        {
-            updateButton.setEnabled(true);
-        }
 
 
 
@@ -386,6 +382,11 @@ public class BudgetItems extends AppCompatActivity {
                 //disable the button after it's clicked
                 addButton.setEnabled(false);
                 updateButton.setEnabled(true);
+
+
+                //immediately go to show activity
+               finish();
+
             }
 
         });
@@ -397,10 +398,8 @@ public class BudgetItems extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                // Fetch input values
 
-                //get the values here.
-                //see how I am NOT grabbing the StartingIncome value here!!!!
-                String IncomeTXT = Income.getText().toString();
                 String RentTXT = Rent.getText().toString();
                 String UtilitiesTXT = Utilities.getText().toString();
                 String PhoneTXT = Phone.getText().toString();
@@ -417,216 +416,108 @@ public class BudgetItems extends AppCompatActivity {
                 String MoreIncomeTXT = MoreIncome.getText().toString();
 
 
-
-                //now we need to check if it is blank. if blank then we need to retrieve the current value in the database.
-                //income this is getting it
-                if (IncomeTXT.isEmpty()) {
-
-                    if (dbAssist.getIncome(userID).isEmpty()) {
-                        IncomeTXT = "0";
-
-                    } else {
-                        IncomeTXT = dbAssist.getIncome(userID);
-                    }
-
-                } else {
-                    IncomeTXT = Income.getText().toString();
-                }
-
-                //rent
-                if (RentTXT.isEmpty()) {
-                    if (dbAssist.getRent(userID).isEmpty()) {
-                        RentTXT = "0";
-                    } else {
-                        RentTXT = dbAssist.getRent(userID);
-                    }
-                } else {
-                    RentTXT = Rent.getText().toString();
-                }
-
-
-                //Utilities
-                if (UtilitiesTXT.isEmpty()) {
-                    if (dbAssist.getUtilities(userID).isEmpty()) {
-                        UtilitiesTXT = "0";
-                    } else {
-                        UtilitiesTXT = dbAssist.getUtilities(userID);
-                    }
-                } else {
-                    UtilitiesTXT = Utilities.getText().toString();
-                }
-
-
-                //phone
-                if (PhoneTXT.isEmpty()) {
-                    if (dbAssist.getPhone(userID).isEmpty()) {
-                        PhoneTXT = "0";
-                    } else {
-                        PhoneTXT = dbAssist.getPhone(userID);
-                    }
-                } else {
-                    PhoneTXT = Phone.getText().toString();
-                }
-
-
-                if (InternetTXT.isEmpty()) {
-                    if (dbAssist.getInternet(userID).isEmpty()) {
-                        InternetTXT = "0";
-                    } else {
-                        InternetTXT = dbAssist.getInternet(userID);
-                    }
-                } else {
-                    InternetTXT = Internet.getText().toString();
-                }
-
-
-                if (GymTXT.isEmpty()) {
-                    if (dbAssist.getGym(userID).isEmpty()) {
-                        GymTXT = "0";
-                    } else {
-                        GymTXT = dbAssist.getGym(userID);
-                    }
-                } else {
-                    GymTXT = Gym.getText().toString();
-                }
-
-
-                if (FoodTXT.isEmpty()) {
-                    if (dbAssist.getFood(userID).isEmpty()) {
-                        FoodTXT = "0";
-                    } else {
-                        FoodTXT = dbAssist.getFood(userID);
-                    }
-                } else {
-                    FoodTXT = Food.getText().toString();
-                }
-
-                if (GasTXT.isEmpty()) {
-                    if (dbAssist.getGas(userID).isEmpty()) {
-                        GasTXT = "0";
-                    } else {
-                        GasTXT = dbAssist.getGas(userID);
-                    }
-                } else {
-                    GasTXT = Gas.getText().toString();
-                }
-
-                if (InsuranceTXT.isEmpty()) {
-                    if (dbAssist.getInsurance(userID).isEmpty()) {
-                        InsuranceTXT = "0";
-                    } else {
-                        InsuranceTXT = dbAssist.getInsurance(userID);
-                    }
-                } else {
-                    InsuranceTXT = Insurance.getText().toString();
-                }
-
-
-                if (CarLoanTXT.isEmpty()) {
-                    if (dbAssist.getCar(userEmail).isEmpty()) {
-                        CarLoanTXT = "0";
-                    } else {
-                        CarLoanTXT = dbAssist.getCar(userID);
-                    }
-                } else {
-                    CarLoanTXT = CarLoan.getText().toString();
-                }
-
-                if (StudentLoanTXT.isEmpty()) {
-                    if (dbAssist.getStudent(userID).isEmpty()) {
-                        StudentLoanTXT = "0";
-                    } else {
-                        StudentLoanTXT = dbAssist.getStudent(userID);
-                    }
-                } else {
-                    StudentLoanTXT = StudentLoan.getText().toString();
-                }
-
-                if (CharityTXT.isEmpty()) {
-                    if (dbAssist.getCharity(userID).isEmpty()) {
-                        CharityTXT = "0";
-                    } else {
-                        CharityTXT = dbAssist.getCharity(userID);
-                    }
-                } else {
-                    CharityTXT = Charity.getText().toString();
-                }
-
-
-                if (EmergencyFundTXT.isEmpty()) {
-                    if (dbAssist.getEmergency(userID).isEmpty()) {
-                        EmergencyFundTXT = "0";
-                    } else {
-                        EmergencyFundTXT = dbAssist.getEmergency(userID);
-                    }
-                } else {
-                    EmergencyFundTXT = EmergencyFund.getText().toString();
-                }
-
-
-                if (SavingsTXT.isEmpty()) {
-                    if (dbAssist.getSavings(userID).isEmpty()) {
-                        SavingsTXT = "0";
-                    } else {
-                        SavingsTXT = dbAssist.getSavings(userID);
-                    }
-                } else {
-                    SavingsTXT = Savings.getText().toString();
-                }
-
-
-                if(MoreIncomeTXT.isEmpty()) {
+                // Handle MoreIncome input
+                if (MoreIncomeTXT.isEmpty()) {
                     MoreIncomeTXT = "0";
                 }
-                else
-                {
-                    MoreIncomeTXT = MoreIncome.getText().toString();
-                }
 
-                //now ensure that the values do not overexceed the income
-                //convert everything to double NOT USED YET
+                // Convert final values to doubles
+                double income = parseDoubleOrZero(dbAssist.getIncome(userID));
+                double rentCurrent = parseDoubleOrZero(dbAssist.getRent(userID));
+                double rent = parseDoubleOrZero(RentTXT);
 
-                //maybe I should put these all into an array and do for loops later to clean it up.
-                double income = Double.parseDouble(IncomeTXT);
-                double rent = Double.parseDouble(RentTXT);
-                double utilities = Double.parseDouble(UtilitiesTXT);
-                double phone = Double.parseDouble(PhoneTXT);
-                double internet = Double.parseDouble(InternetTXT);
-                double gym = Double.parseDouble(GymTXT);
-                double food = Double.parseDouble(FoodTXT);
-                double gas = Double.parseDouble(GasTXT);
-                double insurance = Double.parseDouble(InsuranceTXT);
-                double car = Double.parseDouble(CarLoanTXT);
-                double student = Double.parseDouble(StudentLoanTXT);
-                double charity = Double.parseDouble(CharityTXT);
-                double emergency = Double.parseDouble(EmergencyFundTXT);
-                double savings = Double.parseDouble(SavingsTXT);
-                double moreIncome = Double.parseDouble(MoreIncomeTXT);
+                double utilitiesCurrent = parseDoubleOrZero(dbAssist.getUtilities(userID));
+                double utilities = parseDoubleOrZero(UtilitiesTXT);
 
+                double phoneCurrent = parseDoubleOrZero(dbAssist.getPhone(userID));
+                double phone = parseDoubleOrZero(PhoneTXT);
+
+                double internetCurrent = parseDoubleOrZero(dbAssist.getInternet(userID));
+                double internet = parseDoubleOrZero(InternetTXT);
+
+                double gymCurrent = parseDoubleOrZero(dbAssist.getGym(userID));
+                double gym = parseDoubleOrZero(GymTXT);
+
+                double foodCurrent = parseDoubleOrZero(dbAssist.getFood(userID));
+                double food = parseDoubleOrZero(FoodTXT);
+
+                double gasCurrent = parseDoubleOrZero(dbAssist.getGas(userID));
+                double gas = parseDoubleOrZero(GasTXT);
+
+                double insuranceCurrent = parseDoubleOrZero(dbAssist.getInsurance(userID));
+                double insurance = parseDoubleOrZero(InsuranceTXT);
+
+                double carCurrent = parseDoubleOrZero(dbAssist.getCar(userID));
+                double car = parseDoubleOrZero(CarLoanTXT);
+
+                double studentCurrent = parseDoubleOrZero(dbAssist.getStudent(userID));
+                double student = parseDoubleOrZero(StudentLoanTXT);
+
+                double charityCurrent = parseDoubleOrZero(dbAssist.getCharity(userID));
+                double charity = parseDoubleOrZero(CharityTXT);
+
+                double emergencyCurrent = parseDoubleOrZero(dbAssist.getEmergency(userID));
+                double emergency = parseDoubleOrZero(EmergencyFundTXT);
+
+                double savingsCurrent = parseDoubleOrZero(dbAssist.getSavings(userID));
+                double savings = parseDoubleOrZero(SavingsTXT);
+
+                double moreIncome = parseDoubleOrZero(MoreIncomeTXT);
+
+                // Calculate final income and total expenses
                 double incomeFinal = income + moreIncome;
-                String incomeFinalString = String.valueOf(incomeFinal);
-
                 double total = rent + utilities + phone + internet + gym + food + gas + insurance + car + student + charity + emergency + savings;
-                if (total > income) {
-                    Toast.makeText(BudgetItems.this, "Total exceeds income. Please adjust values and use zeros.", Toast.LENGTH_SHORT).show();
-                    return;
-                } else {
-                    //if we are able to actually update the database without error then we are good.
-                    Boolean checkupdatedata = db.updateData(userID, incomeFinalString, RentTXT, UtilitiesTXT, PhoneTXT, InternetTXT, GymTXT, FoodTXT, GasTXT, InsuranceTXT, CarLoanTXT, StudentLoanTXT, CharityTXT, EmergencyFundTXT, SavingsTXT);
-                    //send a good message
-                    if (checkupdatedata == true) {
-                        Toast.makeText(BudgetItems.this, "Entry Updated ", Toast.LENGTH_SHORT).show();
-                    }
-                    //else if it is not then send a bad message
-                    else {
-                        Toast.makeText(BudgetItems.this, "Entry Not Updated.", Toast.LENGTH_SHORT).show();
-                    }
 
+                double rentFINAL = rentCurrent + rent;
+                double utilitiesFINAL = utilitiesCurrent + utilities;
+                double phoneFINAL = phoneCurrent + phone;
+                double internetFINAL = internetCurrent + internet;
+                double gymFINAL = gymCurrent + gym;
+                double foodFINAL = foodCurrent + food;
+                double gasFINAL = gasCurrent + gas;
+                double insuranceFINAL = insuranceCurrent + insurance;
+                double carFINAL = carCurrent + car;
+                double studentFINAL = studentCurrent + student;
+                double charityFINAL = charityCurrent + charity;
+                double emergencyFINAL = emergencyCurrent + emergency;
+                double savingsFINAL = savingsCurrent + savings;
+
+                // Check if total exceeds income
+                if (total > incomeFinal) {
+                    Toast.makeText(BudgetItems.this, "Total exceeds income. Please adjust values.", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                // Update the database
+                Boolean checkupdatedata = db.updateData(userID, String.valueOf(incomeFinal), String.valueOf(rentFINAL), String.valueOf(utilitiesFINAL), String.valueOf(phoneFINAL),
+                        String.valueOf(internetFINAL), String.valueOf(gymFINAL), String.valueOf(foodFINAL), String.valueOf(gasFINAL), String.valueOf(insuranceFINAL),
+                        String.valueOf(carFINAL), String.valueOf(studentFINAL), String.valueOf(charityFINAL), String.valueOf(emergencyFINAL), String.valueOf(savingsFINAL));
+
+                if (checkupdatedata) {
+                    Toast.makeText(BudgetItems.this, "Entry Updated", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(BudgetItems.this, "Entry Not Updated", Toast.LENGTH_SHORT).show();
+                }
+
+                finish();
             }
 
-        });
+            // Helper methods
+            private String handleEmptyInput(String inputTXT, String dbValue) {
+                if (inputTXT.isEmpty()) {
+                    return dbValue.isEmpty() ? "0" : dbValue;
+                }
+                return inputTXT;
+            }
 
+            private double parseDoubleOrZero(String value) {
+                try {
+                    return Double.parseDouble(value);
+                } catch (NumberFormatException e) {
+                    return 0;
+                }
+            }
+        });
 
 
 
@@ -668,6 +559,8 @@ public class BudgetItems extends AppCompatActivity {
                                 //enable the button once we refresh the database.
                                 addButton.setEnabled(true);
                                 updateButton.setEnabled(false);
+                                Income.setEnabled(true);
+                                MoreIncome.setEnabled(false);
 
                             }
 
